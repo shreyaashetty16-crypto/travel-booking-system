9ac9450ecad464a6a42e6df27afb02f42c9efd9b
shreyaashetty16-crypto <shreyaashetty16@gmail.com>
2026-09-17 11:34:14 +0530

    Update TravelbookingApplication.java

--- backend/src/main/java/com/shreya/travelbooking/TravelbookingApplication.java
+++ backend/src/main/java/com/shreya/travelbooking/TravelbookingApplication.java
@@ -2,12 +2,27 @@ package com.shreya.travelbooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TravelbookingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TravelbookingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TravelbookingApplication.class, args);
+    }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://travel-booking-system-3-uolm.onrender.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
